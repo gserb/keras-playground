c237be10bc9a7e2c760f50f588940eee3d5dc845
George Serbanescu <georgeserbanescu@hotmail.com>
2018-11-04 22:38:45 +0200

    import data from csv to use with a keras model

---
+++ neural_net_csv.py
@@ -0,0 +1,42 @@
+from keras.models import Sequential
+from keras.layers import Dense
+
+import numpy as np
+
+model = Sequential()
+
+
+# 10.5, 5, 9.5, 12 => low
+
+# < 50 = low
+# > 50 = high
+
+model.add(Dense(8, activation='relu', input_dim=4))
+model.add(Dense(16, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(16, activation='relu'))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+# 0 = low
+# 1 = high
+
+model.compile(
+    optimizer='adam', 
+    loss='binary_crossentropy',
+    metrics=['accuracy']
+)
+
+data = np.genfromtxt('high_low.csv', delimiter=',')
+
+x_train = data[1:, :4]
+y_train = data[1:, 4]
+
+model.fit(
+    x_train,
+    y_train,
+    epochs=100,
+    validation_split=0.2
+)
